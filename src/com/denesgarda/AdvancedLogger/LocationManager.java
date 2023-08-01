3fea47ef2ccdd1613b280797adab4f31a71beb05
Denes Garda <dsgarda@gmail.com>
2023-08-01 16:35:26 +0200

    Added dimension differentiator

--- src/com/denesgarda/AdvancedLogger/LocationManager.java
+++ src/com/denesgarda/AdvancedLogger/LocationManager.java
@@ -2,8 +2,11 @@ package com.denesgarda.AdvancedLogger;
 
 import org.bukkit.Location;
 
+import java.util.Objects;
+
 public class LocationManager {
     public static String xyz(Location location) {
-        return "{" + Math.round(location.getX()) + " " + Math.round(location.getY()) + " " + Math.round(location.getZ()) + "}";
+        String world = Objects.requireNonNull(location.getWorld()).getEnvironment().name();
+        return world + "{" + Math.round(location.getX()) + " " + Math.round(location.getY()) + " " + Math.round(location.getZ()) + "}";
     }
 }
