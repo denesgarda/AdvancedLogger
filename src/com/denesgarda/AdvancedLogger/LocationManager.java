154545f45d50360704aeb38db717906ace4aaa1b
Denes Garda <dsgarda@gmail.com>
2023-08-02 11:54:11 +0200

    Update LocationManager.java

--- src/com/denesgarda/AdvancedLogger/LocationManager.java
+++ src/com/denesgarda/AdvancedLogger/LocationManager.java
@@ -7,6 +7,6 @@ import java.util.Objects;
 public class LocationManager {
     public static String xyz(Location location) {
         String world = Objects.requireNonNull(location.getWorld()).getEnvironment().name();
-        return world + "{" + Math.round(location.getX()) + " " + Math.round(location.getY()) + " " + Math.round(location.getZ()) + "}";
+        return world + "{" + Math.floor(location.getX()) + " " + Math.floor(location.getY()) + " " + Math.floor(location.getZ()) + "}";
     }
 }
